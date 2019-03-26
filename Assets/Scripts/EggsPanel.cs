92e71e44de1dc312703f7fb52e6ed669e5aa6d84
unknown <alejandro.suau@gmail.com>
2019-03-26 16:54:11 +0100

    Created Egg Panel configuration for levels

---
+++ Assets/Scripts/EggsPanel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EggColor
+{
+    Yellow,
+    Green,
+    Red,
+    Blue
+}
+
+public class EggsPanel : MonoBehaviour
+{
+    public GameObject[] availableBasicEggs;
+
+    public GameObject[] floors;
+    public GameObject[] eggs;
+    int remainingEggs;
+
+    const float extraEggPositionY =  0.70f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eggs = new GameObject[floors.Length];
+        remainingEggs = 0;
+        SpawnEggs();
+    }
+
+    void SpawnEggs()
+    {
+        for(int i = 0; i < eggs.Length; i++) {
+            Vector3 eggPosition = floors[i].GetComponent<Transform>().position;
+            eggPosition.y += extraEggPositionY;
+
+            eggs[i] = Instantiate(availableBasicEggs[Random.Range(0, availableBasicEggs.Length)],
+                                    eggPosition, 
+                                    Quaternion.identity);
+            remainingEggs ++;
+        }
+    }
+}
