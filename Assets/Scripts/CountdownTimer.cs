442a58d5ec2fe3917e6e853882fb7c159f962501
unknown <alejandro.suau@gmail.com>
2019-03-28 11:38:30 +0100

    Added Canvas, and CountdownTimer to manage the level time

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public Text textTime;
+    public Text textSeconds;
+    
+    [SerializeField]
+    private float startingTime;
+    
+    private float currentTime;
+    private bool isPaused;
+    private float secondElapsing;
+
+    void Start()
+    {
+        currentTime = startingTime;
+        secondElapsing = 0;
+        UpdateTextTime();
+    }
+
+    void Update()
+    {
+        if (!HasEnded() && !isPaused) {
+            currentTime -= Time.deltaTime;
+            
+            secondElapsing += Time.deltaTime;
+            if (secondElapsing >= 1 || currentTime <= 0) {
+                UpdateTextTime();
+                secondElapsing = 0;
+            }
+        }
+    }
+
+    void UpdateTextTime()
+    {
+        textSeconds.text = (Mathf.Ceil(currentTime)).ToString();
+    }
+
+    bool HasEnded()
+    {
+        return currentTime <= 0;
+    }
+
+    void Reset()
+    {
+        currentTime = startingTime;
+    }
+}
