d0ca2106a332f24bcb69bed1e9a9c5910098cb09
unknown <alejandro.suau@gmail.com>
2019-04-05 17:07:09 +0200

    Added pause menu

---
+++ Assets/Scripts/ScreenButtons.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenButtons : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public Button restartLevelButton, pauseButton, playButton, backToLevelsButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //restartLevelButton.onClick.AddListener();
+        pauseButton.onClick.AddListener(Pause);
+        playButton.onClick.AddListener(Play);
+        //backToLevelsButton.onClick.AddListener();
+    }
+
+    void Play()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
